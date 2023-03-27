156eed09f9a77165dc1797bd4fd9ba9050433830
QshoTigura <q.mehandjiiski2019@pmg-pernik.com>
2023-03-27 09:26:54 +0300

    snimki

---
+++ Camera Roll/jasi/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            if (a == "apple" || a == "kiwi" || a == "cherry" || a == "lemon" || a == "grapes")
+            {
+                Console.WriteLine("Fruit");
+            }
+            else if (a == "tomato" || a == "cucumber" || a == "pepper" || a == "carrot")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else
+            {
+                Console.WriteLine("Unknown");
+            }
+        }
+    }
+}
